994c97d3364d56f0385c6830f0aa343f2669bb98
Florian Bruggisser <florian@bildspur.ch>
2020-08-12 10:02:42 +0200

    implemented test script

---
+++ test/serial_test.py
@@ -0,0 +1,48 @@
+import serial
+import time
+
+port = serial.Serial('/dev/tty.usbserial-14410', 115200, timeout=1)
+
+device_id = "30:ae:a4:14:e7:62"
+service_id = "846123f6-ccf1-11ea-87d0-0242ac130003"
+neopixel_color_id = "fc3affa6-5020-47ce-93db-2e9dc45c9b55"
+
+def millis():
+    return int(round(time.time() * 1000))
+
+
+def send_and_read(command, wait_time=1000):
+    print(command)
+    message = "%s\n" % command
+    port.write(message.encode('utf_8'))
+    stime = millis()
+
+    while millis() - stime < wait_time:
+        result = port.readline().decode('utf_8')
+        if result.strip() != "":
+            print(result.strip())
+
+
+send_and_read("connect %s" % device_id)
+
+# test text
+send_and_read("write %s %s %s %s" % (device_id, service_id, neopixel_color_id, "hello"))
+send_and_read("read %s %s %s" % (device_id, service_id, neopixel_color_id))
+
+# test i8
+send_and_read("write %s %s %s %s %s" % (device_id, service_id, neopixel_color_id, 22, "i8"))
+send_and_read("read %s %s %s %s" % (device_id, service_id, neopixel_color_id, "i8"))
+
+# test i16
+send_and_read("write %s %s %s %s %s" % (device_id, service_id, neopixel_color_id, 1000, "i16"))
+send_and_read("read %s %s %s %s" % (device_id, service_id, neopixel_color_id, "i16"))
+
+# test i32
+send_and_read("write %s %s %s %s %s" % (device_id, service_id, neopixel_color_id, 0x00FFFF, "i32"))
+send_and_read("read %s %s %s %s" % (device_id, service_id, neopixel_color_id, "i32"))
+
+time.sleep(5)
+
+send_and_read("disconnect %s" % device_id)
+
+port.close()
